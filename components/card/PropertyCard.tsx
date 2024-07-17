8bb585e2f8efe9d6b5f0b86877e1377cce7adfe5
todayIsDecember <timkivit@gmail.com>
2024-07-17 22:11:02 +0300

    add review

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -25,6 +25,7 @@ function PropertyCard({property}: {property: PropertyCardProps}) {
                 <div className='flex justify-between items-center'>
                     <h3 className='text-sm font-semibold mt-1'>{name.substring(0, 30)}</h3>
                     {/* property rating */}
+                    <PropertyRating inPage={false} propertyId={PropertyId} />
                 </div>
                 <p className='text-sm mt-1 text-muted-foreground'>
                     {tagline.substring(0, 40)}
--- components/card/PropertyRating.tsx
+++ components/card/PropertyRating.tsx
@@ -1,6 +1,21 @@
-function PropertyRating() {
+import {FaStar} from 'react-icons/fa'
+
+function PropertyRating({propertyId, inPage}: {propertyId: string, inPage: boolean}) {
+    const raiting = 4.7
+    const count = 100
+
+    const className = `flex items-center gap-1 ${inPage? 'text-md': 'text-xs'}`;
+
+    const countText = count > 1 ? 'reviews' : 'review';
+
+    const countValue = ` (${count}) ${inPage ? countText: ''}`
+
     return (
-        <div>PropertyRating</div>
+        <span className={className}>
+            <FaStar className='h-3 w-3' />
+            {raiting}
+            {countValue}
+        </span>
     )
 }
 
